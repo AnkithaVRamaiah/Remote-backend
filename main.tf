dd850edcfefcef16cd129f6b75cd65d25b0ca28a
AnkithaVRamaiah <113353061+AnkithaVRamaiah@users.noreply.github.com>
2024-08-29 09:02:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "ankitha" {
   instance_type = "t2.micro"
   ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
